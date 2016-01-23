7970997f11594a90f0b50c48b5ed3565055a1174
sheepforce <phillip.seeber@gmx.de>
2016-01-24 00:26:56 +0100

    Debugging of RK_SOLV
    	- rk_solv.f90: FUNCTION DiffEquation broken, produces incorrect results, adding debug output, disabling integration for debugging
    	- test_rk_solv.f90: initiate with reaction A + B <=> C + D

--- rk_solv/rk_solv.f90
+++ rk_solv/rk_solv.f90
@@ -56,10 +56,54 @@ INTERFACE
 
                 INCLUDE 'rk_solv.fh'
 
-                REAL*8 :: ConcMultiEduct, ConcMultiProduct, SumEduct, SumProd                           ! interim results of ODE
+                REAL*8 :: ConcMultiUse, ConcMultiProduce, SumUse, SumProduce                           ! interim results of ODE
         END FUNCTION DiffEquation
 END INTERFACE
 
+
+!!=============!!
+!! START DEBUG !!
+!!=============!!
+WRITE(*, *) "======================================================="
+WRITE(*, *) "|| Here is RK_SOLV. Numerical solving the ODE system ||"
+WRITE(*, *) "======================================================="
+WRITE(*, *)
+WRITE(*, *)
+WRITE(*, *) "Punching some values for debug purposes now"
+WRITE(*, *) "-------------------------------------------"
+WRITE(* ,*) 
+WRITE(*, *) "== 0-dimensional =="
+WRITE(*, *) "NUMTHREAD     ", NUMTHREAD
+WRITE(*, *) "method        ", method
+WRITE(*, *) "finaltime     ", finaltime
+WRITE(*, *) "dtime         ", dtime
+WRITE(*, *)
+WRITE(*, *) "== 1-dimensional =="
+WRITE(*, *) "ConcVec with size", SIZE(ConcVec)
+WRITE(*, *) "        ", ConcVec(1:)
+WRITE(*, *)
+WRITE(*, *) "RateVec with size", SIZE(RateVec)
+DO i = 1, SIZE(RateVec)
+        WRITE(*, *) "        ", RateVec(i)
+END DO
+WRITE(*, *)
+WRITE(*, *) "== 2-dimensional =="
+WRITE(*, *) "ProdMat with size", SIZE(ProdMat, 1), "x", SIZE(ProdMat, 2)
+DO i = 1, SIZE(ProdMat, 1)
+        WRITE(*, *) "        ", ProdMat(i,:)
+END DO
+WRITE(*, *)
+WRITE(*, *) "EdMat with size", SIZE(EdMat, 1), "x", SIZE(EdMat, 2)
+DO i = 1, SIZE(EdMat, 1)
+        WRITE(*, *) "        ", EdMat(i,:)
+END DO
+
+DO i = 1, 5
+	WRITE(*, *)
+END DO
+!!=============!!
+!!  END DEBUG  !!
+!!=============!!
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!
 !! INITIALIZATION PHASE !!
@@ -69,6 +113,18 @@ Omega = SIZE(ConcVec)                                                           
 N = SIZE(RateVec)                                                                                       ! determines number of reactions
 NSteps = NINT(finaltime / dtime)                                                                        ! Number of steps, calculated by time to analyse and time step length
 
+!!=============!!
+!! START DEBUG !!
+!!=============!!
+WRITE(*, *) "Omega is", Omega
+WRITE(*, *) "N is", N
+
+DO i = 1, 5
+        WRITE(*, *)
+END DO
+!!=============!!
+!!  END DEBUG  !!
+!!=============!!
 ALLOCATE(IntConcMat(1:Omega,1:2))                                                                       ! allocate matrix, that stores input and ouput of integrator step
 
 
@@ -78,33 +134,64 @@ ALLOCATE(IntConcMat(1:Omega,1:2))                                               
 
 OPEN(UNIT=100, FILE="Concentration.dat")                                                                ! opens output file
 
+!!=============!!
+!! START DEBUG !!
+!!=============!!
+WRITE(*, *) "parallel integration routine is launchin now"
+WRITE(*, *) "--------------------------------------------"
+WRITE(*, *)
+WRITE(*, *)
+!!=============!!
+!!  END DEBUG  !!
+!!=============!!
+
 ! initialize IntConcMat for intial conditions at t = 0
-!$OMP PARALLEL NUM_THREADS(NUMTHREAD)
+!!!$OMP PARALLEL NUM_THREADS(NUMTHREAD)
+
+!!=============!!
+!! START DEBUG !!
+!!=============!!
+WRITE(*, *) "intial ODE calculation, calling function DiffEquation, OpenMP thread", omp_get_thread_num()
+!!=============!!
+!!  END DEBUG  !!
+!!=============!!
 DO lambda = 1, Omega
         IntConcMat(lambda,1) = DiffEquation(EdMat, ProdMat, RateVec, ConcVec, lambda)                   ! Calculate Value of ODE at initial conditions, t = 0
         IntConcMat(lambda,2) = 0
 END DO
-!$OMP END PARALLEL
+!!!$OMP END PARALLEL
 
-DO IntStep = 1, NSteps                                                                                  ! Integrate NSteps times
-        !$OMP PARALLEL NUM_THREADS(NUMTHREAD)
-        DO lambda = 1, Omega                                                                            ! Iterate over every substance before doing next integration step
-                IF (method == 1) THEN                                                                   ! Euler Integrator
-                        IntConcMat(lambda,2) = IntConcMat(lambda,1) - dtime * IntConcMat(lambda,1)
-                END IF
-
-        END DO
-        !$OMP END PARALLEL
-
-        WRITE(UNIT=100, FMT='(D10.5, 4X)') dtime * IntStep, (IntConcMat(lambda,2), lambda = 1, Omega)   ! write result of this round to output file
-	
-	!$OMP PARALLEL NUM_THREADS(NUMTHREAD)
-	DO lambda = 1, Omega
-		IntConcMat(lambda,1) = DiffEquation(EdMat, ProdMat, RateVec, IntConcMat(:,2), lambda)	! calculate new value of differential equation with integatred values
-	END DO
-	!$OMP END PARALLEL
-
+!!=============!!
+!! START DEBUG !!
+!!=============!!
+WRITE(*, *)
+WRITE(*, *) "Initial ODE values stored in IntConcMat(1,:). IntConcMat(2,:) initialized with 0. Here is IntConcMat"
+DO i = 1, SIZE(IntConcMat, 1)
+	WRITE(*, *) IntConcMat(i,1:)
 END DO
+!!=============!!
+!!  END DEBUG  !!
+!!=============!!
+
+!DO IntStep = 1, NSteps                                                                                  ! Integrate NSteps times
+!        !$OMP PARALLEL NUM_THREADS(NUMTHREAD)
+!        DO lambda = 1, Omega                                                                            ! Iterate over every substance before doing next integration step
+!                IF (method == 1) THEN                                                                   ! Euler Integrator
+!                        IntConcMat(lambda,2) = IntConcMat(lambda,1) - dtime * IntConcMat(lambda,1)
+!                END IF
+!
+!        END DO
+!        !$OMP END PARALLEL
+!
+!        WRITE(UNIT=100, FMT='(D10.5, 4X)') dtime * IntStep, (IntConcMat(lambda,2), lambda = 1, Omega)   ! write result of this round to output file
+!	
+!	!$OMP PARALLEL NUM_THREADS(NUMTHREAD)
+!	DO lambda = 1, Omega
+!		IntConcMat(lambda,1) = DiffEquation(EdMat, ProdMat, RateVec, IntConcMat(:,2), lambda)	! calculate new value of differential equation with integatred values
+!	END DO
+!	!$OMP END PARALLEL
+!
+!END DO
 
 CLOSE(UNIT=100)
 
@@ -123,20 +210,52 @@ IMPLICIT NONE
 
 INCLUDE 'rk_solv.fh'
 
-REAL*8 :: ConcMultiEduct, ConcMultiProduct, SumEduct, SumProd                                           ! interim results of ODE
+REAL*8 :: ConcMultiConsume, ConcMultiProduce, SumConsume, SumProduce					! interim results of ODE
+
+!!=============!!
+!! START DEBUG !!
+!!=============!!
+WRITE(*, *) "================================"
+WRITE(*, *) "|| FUNCTION DiffEquation here ||"
+WRITE(*, *) "================================"
+WRITE(*, *)
+WRITE(*, *) "*** SubsNum", SubsNum, "***"
+!!=============!!
+!!  END DEBUG  !!
+!!=============!!
+
 
 Omega = SIZE(ConcVec)                                                                                   ! determines number of substances
-N = SIZE(RateVec)                                                                                       ! determines number of reactions
+N = SIZE(RateVec)
 
+ConcMultiConsume = 1.0d0										! neutral values regarding their operation
+ConcMultiProduce = 1.0d0
+SumConsume = 0.0d0
+SumProduce = 0.0d0
+
+! consuming part of ODE
 DO I = 1, N
-        DO lambda = 1, Omega
-                ConcMultiEduct = ConcVec(lambda) ** (EdMat(I,lambda)) * ConcMultiEduct                  ! calculates product of concentrations^(stochiometric coefficient on educt side)
-                ConcMultiProduct = ConcVec(lambda) ** (ProdMat(I,lambda)) * ConcMultiProduct            ! calculates product of concentrations^(stochiometric coefficient on product side)
-        END DO
-
-        SumEduct = RateVec(I) * EdMat(I,SubsNum) * ConcMultiEduct                                       ! calculates sum of rate constant of reaction I * stochiometric coefficient of 
-        SumProd = RateVec(I) * ProdMat(I,SubsNum) * ConcMultiProduct                                    ! substance SubsNum * ConcMulti... --> yealding consuming and producing part of the right side DE
+	DO lambda = 1, Omega
+		ConcMultiConsume = ( ConcVec(lambda) ** EdMat(I,lambda) ) * ConcMultiConsume		! calculating the product term of consuming part of the ODE
+	END DO
+	SumConsume = RateVec(I) * EdMat(I,SubsNum) * ConcMultiConsume + SumConsume			! calculating the sum term (over products) of the consuming part of the ODE
+	
+	ConcMultiConsume = 1.0d0									! reset product term for next loop
 END DO
-DiffEquation = - SumEduct + SumProd
+WRITE(*, *) "SumConsume is", SumConsume
+
+
+! producing part of ODE
+DO I = 1, N
+	DO lambda = 1, Omega
+		ConcMultiProduce = ( ConcVec(lambda) ** ProdMat(I,lambda) ) * ConcMultiProduce		! calculating the product term of producing part of the ODE
+	END DO
+	SumProduce = RateVec(I) * ProdMat(I,lambda) * ConcMultiProduce + SumProduce			! calculating the sum term (over products) of the producing part of the ODE
+
+	ConcMultiProduce = 1.0d0
+END DO
+WRITE(*, *) "SumProduce is", SumProduce
+
+DiffEquation = - SumConsume + SumProduce
 
 END FUNCTION DiffEquation
--- rk_solv/test_rk_solv.f90
+++ rk_solv/test_rk_solv.f90
@@ -3,22 +3,36 @@
 PROGRAM TEST_RK_SOLV
 IMPLICIT NONE
 
-INTEGER, DIMENSION(2,3) :: EdMat, ProdMat
-REAL*8, DIMENSION(3) :: ConcVec
+INTEGER, DIMENSION(2,4) :: EdMat, ProdMat
+REAL*8, DIMENSION(4) :: ConcVec
 REAL*8, DIMENSION(2) :: RateVec
 REAL*8 :: dtime = 0.5d0, finaltime=1.0d3
 INTEGER :: method = 1, NUMTHREAD = 4, i
+
+INTERFACE
+	SUBROUTINE RK_SOLV(EdMat, ProdMat, RateVec, ConcVec, dtime, finaltime, method, NUMTHREAD)
+		USE omp_lib                                                                                             ! OMP definitions
+
+		IMPLICIT NONE
+
+		INCLUDE 'rk_solv.fh'                                                                                    ! include type declarations
+
+		REAL*8 ::  DiffEquation
+	END SUBROUTINE
+END INTERFACE
 
 WRITE(*, *) "================================================================================"
 WRITE(*, *) "|| Here is TEST_RK_SOLV. Testing the routine RK_SOLV with some initial values ||"
 WRITE(*, *) "================================================================================"
 
 EdMat(1,1) = 1
-EdMat(1,2) = 0
+EdMat(1,2) = 1
 EdMat(1,3) = 0
+EdMat(1,4) = 0
 EdMat(2,1) = 0
-EdMat(2,2) = 1
-EdMat(2,3) = 0
+EdMat(2,2) = 0
+EdMat(2,3) = 1
+EdMat(2,4) = 1
 
 WRITE(*, *)
 WRITE(*, *) "EdMat"
@@ -30,11 +44,13 @@ WRITE(*, *)
 
 
 ProdMat(1,1) = 0
-ProdMat(1,2) = 1
-ProdMat(1,3) = 0
-ProdMat(2,1) = 0
-ProdMat(2,2) = 0
-ProdMat(2,3) = 1
+ProdMat(1,2) = 0
+ProdMat(1,3) = 1
+ProdMat(1,4) = 1
+ProdMat(2,1) = 1
+ProdMat(2,2) = 1
+ProdMat(2,3) = 0
+ProdMat(2,4) = 0
 
 WRITE(*, *)
 WRITE(*, *) "ProdMat"
@@ -45,16 +61,17 @@ WRITE(*, *)
 
 
 ConcVec(1) = 10.0d0
-ConcVec(2) = 0.0d0
-ConcVec(3) = 0.0d0
+ConcVec(2) = 4.0d0
+ConcVec(3) = 2.0d0
+ConcVec(4) = 0.2d0
 
 WRITE(*, *)
 WRITE(*, *) "ConcVec"
 WRITE(*, *) "        ", ConcVec(1:)
 
 
-RateVec(1) = 0.01d0
-RateVec(2) = 0.0075d0
+RateVec(1) = 0.005d0
+RateVec(2) = 0.01d0
 
 WRITE(*, *)
 WRITE(*, *) "RateVec"
