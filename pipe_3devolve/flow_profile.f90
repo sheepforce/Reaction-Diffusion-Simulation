201d89c10b859fbb42f5b0c3ca0f7f8433bb6baf
Phillip Seeber <phillip@yavin.fritz.box>
2016-01-26 21:36:46 +0100

    working FLOW_PROFILE function and interface to this function in the test
    	- Makefile: added rule for test_pipe_3devolce
    	- flow_profile.f90: removed unecessary data
    	- test_pipe_3devolve.f90: interfaced FLOW_PROFILE and gerating test data

--- pipe_3devolve/flow_profile.f90
+++ pipe_3devolve/flow_profile.f90
@@ -1,11 +1,11 @@
 ! The FLOW_PROFILE function gives the speed of flow on a given point on the XY Grid
 !
 
-REAL*8 FUNCTION FLOW_PROFILE(PointX, PointY, vmax, vadd, PipeRadius, NGridXY)
+REAL*8 FUNCTION FLOW_PROFILE(PointX, PointY, vmax, vadd, NGridXY)
 
 IMPLICIT NONE
 INTEGER, INTENT(IN) :: PointX, PointY, NGridXY
-REAL*8, INTENT(IN) :: vmax, vadd, PipeRadius
+REAL*8, INTENT(IN) :: vmax, vadd
 
 
 FLOW_PROFILE = - vmax * (PointX ** 2 + PointY ** 2) + vmax * NGridXY ** 2 + vadd
--- pipe_3devolve/test_pipe_3devolve.f90
+++ pipe_3devolve/test_pipe_3devolve.f90
@@ -1,5 +1,28 @@
 PROGRAM TEST_PIPE_3DEVOLVE
 IMPLICIT NONE
+INTEGER :: NGridXY, PointX, PointY, a, b
+REAL*8 :: vmax, vadd, PipeRadius, FLOW_PROFILE
+REAL*8, DIMENSION(:,:), ALLOCATABLE :: FlowMatrix
 
+
+NGridXY = 5
+vmax = 1.0d0
+vadd = 0.0d0
+
+ALLOCATE(FlowMatrix(-NGridXY:+NgridXY,-NGridXY:+NGridXY))
+
+DO a = -NgridXY, NGridXY
+	DO b = -NgridXY, NGridXY
+		WRITE(*, *) "Now on point", a, b
+		FlowMatrix(a,b) = FLOW_PROFILE(a, b, vmax, vadd, NGridXY)
+	END DO
+END DO
+
+
+WRITE(*, *) "Here is the FlowMatrix"
+WRITE(*, *)
+DO a = -NgridXY, NGridXY
+	WRITE(*, *) FlowMatrix(a,-NgridXY:)
+END DO
 
 END PROGRAM
